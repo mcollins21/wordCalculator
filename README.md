6b3d94c51a192acb0f1a6a3d2cf4de77700d50ee
Michael Collins <mcollins21@luc.edu>
2023-04-19 20:46:15 -0500

    Implemented initial user prompt and creates list of operations.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# wordCalculator
+## wordCalculator
 
 A functioning calculator that prompts the user to select their operation and then numerical values. This is able to perform simple operations such as addition, subtraction, multiplication, and division.
+
+<strong> Goal: "Demonstrates the importance of how code design must be simplistic and not redundant in nature. </Strong>
--- wordCalculator.java
+++ wordCalculator.java
@@ -1,7 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class wordCalculator {
     public static void main(String[] args) {
 
         System.out.println("Welcome to the Word Calculator... \nPlease type in the operation you would like to perform. ");
+
+        Scanner typeOpp = new Scanner(System.in);  //initializes Scanner typeOpp
+
+        System.out.print("\n");
+
+        ArrayList<String> oppList = new ArrayList<String>();    //initializes oppList and adds operations
+        oppList.add("1. Addition");
+        oppList.add("2. Subtraction");
+        oppList.add("3. Multiplication");
+        oppList.add("4. Division");
+
+/*
+        for(String element : oppList){  //prints out the list 4 times
+            System.out.println(oppList);
+        }
+*/
+        System.out.println(oppList.toString());         //prints out the list of calculator operations
+        System.out.println("\nPlease type what operation you would like to perform");
+
+        String selectOpp = typeOpp.nextLine();
+
+        System.out.println("\n");
+        System.out.print("You have selected: " + selectOpp);        //prints out what user has selected and confirms choice
+
+        System.out.println("\nIs this the correct operation? Y/N to continue... ");
+        Scanner confirmChoice = new Scanner(System.in);
+
 
     }
 }
